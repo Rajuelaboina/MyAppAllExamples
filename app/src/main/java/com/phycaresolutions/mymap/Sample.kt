983bf0ab2b628adbf0a7f9d1036bf60e4e16fba3
rajuelaboina <raju.elaboina@gmail.com>
2024-12-20 20:31:56 +0530

    kotlin higher Order functions    20/12/2024

--- app/src/main/java/com/phycaresolutions/mymap/Sample.kt
+++ app/src/main/java/com/phycaresolutions/mymap/Sample.kt
@@ -1,5 +1,7 @@
 package com.phycaresolutions.mymap
 
+import kotlin.Result
+
 class Sample : OnItemClickListener {
     override fun onItemClick() {
         println("this is interface")
@@ -7,6 +9,12 @@ class Sample : OnItemClickListener {
 
 }
 fun main(){
+    val list:List<Int> = listOf(1,2,3)
+    println(list)
+    val ls:MutableList<Int> = mutableListOf(1,2,3)
+    println(ls)
+    ls[0] = 10
+    println(ls)
     val sample = Sample();
     sample.onItemClick()
 
@@ -15,7 +23,12 @@ fun main(){
     val ev:(Int)->Unit = {x:Int-> print(x) }
     //even(2,ev) // this is first way
     even(2){x:Int-> if (x%2 == 0) println("$x  is Even Number") else println("$x  is Not Even Number") } // second way
+   /* val sum = { a: Int, b: Int -> a + b } // Lambda expression
+    val result = sum(3, 4) // Invoking the lambda expression
+    println(result) // Output: 7*/
+
    // val add = {x:Int,y:Int-> x+y}
+    // result22 = add(10,20)
    // calculate(10,20,add)
     // OR
    // calculate(10,20){x:Int,y:Int->  println(x+y)}
@@ -43,4 +56,18 @@ interface OnItemClickListener{
 }
 abstract class onItemLongClickLinstener{
     abstract fun  onLongClick()
+}
+sealed class Result20{
+   data class Success(val data:String) : Result20()
+  data class Error(val message:String ) : Result20()
+   object Loding : Result20()
+
+}
+fun progressResult(result20: Result20){
+    when(result20){
+        is Result20.Success ->{}
+        is Result20.Error ->{}
+        is Result20.Loding ->{}
+        else -> {}
+    }
 }
