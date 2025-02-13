792072187ae44e2a7060988306e1afe7faf2fcd4
rajuelaboina <raju.elaboina@gmail.com>
2025-02-13 19:37:35 +0530

    LoginActivity.java

--- app/src/main/java/com/phycaresolutions/mymap/LoginActivity.java
+++ app/src/main/java/com/phycaresolutions/mymap/LoginActivity.java
@@ -1,58 +1,87 @@
 package com.phycaresolutions.mymap;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.util.concurrent.Semaphore;
+import com.phycaresolutions.mymap.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final int MAX_THREADS = 5;
-    Object lock = new Object();
-    private int counter = 0;
-    TextView textView;
-    Semaphore semaphore = new Semaphore(MAX_THREADS);
-    ListView listView;
+     ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        textView = findViewById(R.id.textView);
-        listView = findViewById(R.id.listView);
-       String[] str = {"a","b","d","e","f","g","h","i"};
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,str);
-        listView.setAdapter(adapter);
-    }
-    public void counterIncrement(View view){
-       /* new Thread(()->{
-            try {
-               semaphore.acquire();
-               synchronized (lock){
-                   counter++;
-               }
-            }catch (InterruptedException e){
+        EdgeToEdge.enable(this);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-            }finally {
-                 semaphore.release();
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidationUserName(binding.nameEditText.getText().toString().trim())){
+                      binding.textInputLayout2.setError("Enter valid name !");
+                      binding.textInputLayout2.setBoxStrokeErrorColor(ColorStateList.valueOf(Color.RED));
+                }else if (!isValidationPassword(binding.passwordEditText.getText().toString().trim())){
+                    binding.textInputLayout3.setError("Enter valid password! OR password must be greater than 3");
+                    binding.textInputLayout3.setBoxStrokeErrorColor(ColorStateList.valueOf(Color.RED));
+                }else {
+                    Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                    // call to Intent
+                }
             }
-            updateTextView();
-
-        }).start();*/
-        updateTextView();
+        });
+        binding.nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().length()>0){
+                    binding.textInputLayout2.setErrorEnabled(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        binding.passwordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().length()>0){
+                    binding.textInputLayout3.setErrorEnabled(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
-    private void updateTextView() {
-        new Handler(getMainLooper()).post(()-> textView.setText(String.valueOf(counter)));
+    private boolean isValidationPassword(String password) {
+        if (password==null || password.isEmpty()){
+            return false;
+        }else if (password.length()<3){
+            return false;
+        }
+        return true;
     }
+
+    private boolean isValidationUserName(String name) {
+        if (name==null || name.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+
 }
